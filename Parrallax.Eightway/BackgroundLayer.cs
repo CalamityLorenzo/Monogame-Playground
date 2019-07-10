81b74df8e173887fb564c2159c2ab4128cb705a9
paullawrence <Lawrence_pau@hotmail.co.uk>
2019-07-10 20:57:04 +0100

    Did a thing

--- Parrallax.Eightway/BackgroundLayer.cs
+++ Parrallax.Eightway/BackgroundLayer.cs
@@ -17,7 +17,8 @@ namespace Parrallax.Eightway
         private readonly Texture2D[] images;
         private Point frameDimensions;
         private Rotator currentRotation;
-        private readonly Vector2 startOffset;
+        private readonly int _velocity;
+        private Vector2 _currentPosition;
         private readonly Rectangle _drawRange;
         private readonly Rectangle _destination; // The area we draw too. (Should effectively be the viewingport)
         private readonly Rectangle _sourceArea;
@@ -28,10 +29,11 @@ namespace Parrallax.Eightway
             this.images = images;
             frameDimensions = images.Length > 0 ? new Point(images[0].Width, images[0].Height) : Point.Zero;
             this.currentRotation = roation;
+            this._velocity = velocity;
             // Where we start in relation to our frames. so 0,0 means the top left of the first texture is at 0,0on the screen,
             // 50,50 would be :put texture starting at 50,50 at 0,0,
             // ie whats' the starting co-ordinate for the top-left of the screen.
-            this.startOffset = startOffset; 
+            this._currentPosition = startOffset; 
             this._drawRange = spriteBatch.GraphicsDevice.Viewport.Bounds;
             _destination = new Rectangle(0, 0, _drawRange.Width, _drawRange.Height);
         }
@@ -41,6 +43,9 @@ namespace Parrallax.Eightway
             var delta = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
             // calculate drawing start vector
            var directionVector =  GeneralExtensions.UnitAngleVector(90);
+            // now updated our start position (Where on the textture we are drawing ) to pass to the rectangle
+            this._currentPosition += directionVector * _velocity * delta;
+            
 
         }   
 
