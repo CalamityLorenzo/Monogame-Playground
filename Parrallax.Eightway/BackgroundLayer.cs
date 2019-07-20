3b965cb79d79ae3dd14e2aead26e511595204382
paullawrence <Lawrence_pau@hotmail.co.uk>
2019-07-20 08:49:24 +0100

    Mimmis

--- Parrallax.Eightway/BackgroundLayer.cs
+++ Parrallax.Eightway/BackgroundLayer.cs
@@ -37,12 +37,13 @@ namespace Parrallax.Eightway
             this._drawRange = spriteBatch.GraphicsDevice.Viewport.Bounds;
             _destination = new Rectangle(0, 0, _drawRange.Width, _drawRange.Height);
         }
+
         public void Update(GameTime gameTime)
         {
             // elasped since last update.
             var delta = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
             // calculate drawing start vector
-           var directionVector =  GeneralExtensions.UnitAngleVector(90);
+            var directionVector = GeneralExtensions.UnitAngleVector(90);
 
             // Our screen is a window
             // This is where the window top left points in relation to our background.
@@ -55,14 +56,14 @@ namespace Parrallax.Eightway
             var totalWidth = frameDimensions.X * images.Length;
             var totalHeight = frameDimensions.Y;
             // Stay in back ground frame;
-            if(_destinationViewPosition.X> totalWidth)
+            if (_destinationViewPosition.X > totalWidth)
             {
                 _destinationViewPosition = new Vector2(_destinationViewPosition.X - totalWidth, _destinationViewPosition.Y);
             }
 
-            if (_destinationViewPosition.Y>totalHeight)
+            if (_destinationViewPosition.Y > totalHeight)
             {
-                _destinationViewPosition = new Vector2(_destinationViewPosition.X, _destinationViewPosition.Y- totalHeight);
+                _destinationViewPosition = new Vector2(_destinationViewPosition.X, _destinationViewPosition.Y - totalHeight);
             }
 
             // Now we take it down to the background image level
@@ -70,7 +71,7 @@ namespace Parrallax.Eightway
             // now we need to know 1. Which image in the array 2. Where we start.
             // we need to fill up the destination rectangle
             // but we may have to draw 1,2 or 4 rectangles to achieve this.
-            var sourceRectsComplete= false;
+            var sourceRectsComplete = false;
             var _sourceRects = new Rectangle[4];
 
             // we move along this iterator
@@ -84,28 +85,21 @@ namespace Parrallax.Eightway
                 // Create Rectangle
                 var x = frameDimensions.X - _destinationCursor.X;
                 var y = frameDimensions.Y - _destination.Y; // Total height of frame - target to get Y Position.
-                
+
                 var sourceWidth = _destination.Width >= images[imageId].Width - x ? images[imageId].Width - x : _destination.Width;
                 var sourceHeight = _destination.Height >= images[imageId].Height - y ? images[imageId].Height - y : _destination.Height;
 
-                var rectSection = new Rectangle(x,y, sourceWidth ,sourceHeight);
+                var rectSection = new Rectangle(x, y, sourceWidth, sourceHeight);
 
                 if (x + sourceWidth >= _destination.Width && x + sourceHeight >= _destination.Height)
                     sourceRectsComplete = true;
                 if (!sourceRectsComplete)
                     _destinationCursor = new Vector2(_destinationCursor.X + sourceWidth, _destinationCursor.Y + sourceHeight);
 
-            // Okay now find the area we are mapping.
-            // Take the _currentPosition, and find where it in the array.
 
             }
 
 
-                // if the length of x draws over the edge, scale it back to what we can draw
-                float drawWidth = (_currentPosition.X + this._drawRange.Width) > totalWidth ? totalWidth - _currentPosition.X : _currentPosition.X + this._drawRange.Width;
-                float drawHeight = (_currentPosition.Y + this._drawRange.Height) > totalHeight ? totalHeight - _currentPosition.Y : _currentPosition.Y + this._drawRange.Height;
-                this._destination = new Rectangle(_currentPosition.ToPoint(), new Point((int)drawWidth, (int)drawHeight));
-            }
         }
 
         public void Draw()
